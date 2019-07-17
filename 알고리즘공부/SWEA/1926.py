2b036d878fac4a0699b85bcd795611790c0402e7
ByeongjunCho <jjgk91@naver.com>
2019-07-17 23:55:44 +0900

    파스칼 삼각형까지 했음

---
+++ 알고리즘공부/SWEA/1926.py
@@ -0,0 +1,20 @@
+# 369게임
+
+num = int(input())
+
+def change_369(n):
+    tmp = ''
+    if '3' in str(n) or '6' in str(n) or '9' in str(n):
+        for s in list(str(n)):
+            if s in ['3', '6', '9']:
+                tmp += '-'
+        else: 
+            return tmp
+    else:
+        return str(n)
+
+
+# print(change_369(14))
+
+for n in range(1, num + 1):
+    print(change_369(n), end=' ')
