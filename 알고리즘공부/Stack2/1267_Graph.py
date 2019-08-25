a4dcbf92f67c174906339f108611a6445cf9d78b
ByeongjunCho <jjgk91@naver.com>
2019-08-25 16:35:03 +0900

    문제풀이

--- 알고리즘공부/Stack2/1267_Graph.py
+++ 알고리즘공부/Stack2/1267_Graph.py
@@ -1,76 +1,49 @@
 # 1267. [S/W 문제해결 응용] 10일차 - 작업순서
-
 import sys
 sys.stdin = open('1267_input.txt', 'r')
 
 for tc in range(1, 11):
     V, E = map(int, input().split())
     G = [[] for _ in range(V+1)]
+    start_case = set(range(1, V+1)) # 시작값
+    input_count = [0] * (V+1) # 각 노드에 들어오는 엣지 수
     caseE = list(map(int, input().split()))
+
     for i in range(E):
         G[caseE[i << 1]].append(caseE[(i << 1)+1])
-    visit = [False] * (V+1)
-    # 전처리
+        start_case -= {caseE[(i << 1)+1]}
+        input_count[caseE[(i << 1)+1]] += 1
+    visit = [0] * (V+1)
 
-    start_case = []
-
-    output_count = list(map(len, G))
-    # for i in range(len(output_count)):
-    #     if not output_count[i]:
-    #         output_count[i] = 1
-
-    for i in range(1, V+1):
-        for k in G:
-            if i in k:
-                break
-        else:
-            start_case.append(i)
-    input_count = []
-    for j in range(0, V+1):
-        count = 0
-        for k in G:
-            if j in k:
-                count += 1
-        input_count.append(count)
-
-    print(G)
-    print(start_case)
-    print(input_count)
-    print(output_count)
-
-    result = ''
     v = start_case.pop()
-    visit[v] = True
-    tmp_visit = []
+    visit[v] = 1
     S = [v]
-    signal = 0
-    while start_case and S:
-        for w in G[v]:
-            if not visit[w]:
-                #
-                #     continue
-                if input_count[w] > 1:
-                    visit[w] = True
-                    tmp_visit.append(w)
-                    v = S.pop()
-                    input_count[w] -= 1
-                    continue
+    result = [str(v)]
+    while S or start_case:
+        if not S:
+            v = start_case.pop()
+            result.append(str(v))
+            visit[v] = 1
+            S = [v]
 
-                print('{} -> {}'.format(v, w))
-                visit[w] = True
-                S.append(w)
+        for i in range(len(G[v])):
+            w = G[v][i]
+            if visit[w] + 1 == input_count[w] and w:
+                # if str(w) not in result:
+                #     result.append(str(w))
+                result.append(str(w))
+                visit[w] += 1
+                S.append(v)
                 v = w
                 break
+            elif visit[w]+1 != input_count[w] and input_count[w] > 1 and w:
+                visit[w] += 1
+                G[v][i] = 0
         else:
-            if not S:
-                v = start_case.pop()
-                S = [v]
-                for val in tmp_visit:
-                    visit[val] = not visit[val]
-            else:
-                v = S.pop()
-                for val in tmp_visit:
-                    visit[val] = not visit[val]
+            v = S.pop()
+
+    print('#{} {}'.format(tc, ' '.join(result)))
+    print(len(result))
 
 
 
--- 알고리즘공부/Stack2/asdf.py
+++ 알고리즘공부/Stack2/asdf.py
@@ -1,57 +1,81 @@
 # 1267. [S/W 문제해결 응용] 10일차 - 작업순서
-# import sys
-# sys.stdin = open('1267_input.txt', 'r')
+import sys
+sys.stdin = open('1267_input.txt', 'r')
 
 for tc in range(1, 11):
     V, E = map(int, input().split())
     G = [[] for _ in range(V+1)]
+    start_case = set(range(1, V+1)) # 시작값
+    input_count = [0] * (V+1) # 각 노드에 들어오는 엣지 수
     caseE = list(map(int, input().split()))
+
     for i in range(E):
         G[caseE[i << 1]].append(caseE[(i << 1)+1])
+        start_case -= {caseE[(i << 1)+1]}
+        input_count[caseE[(i << 1)+1]] += 1
     visit = [0] * (V+1)
 
-    start_case = []
-    for i in range(1, V+1):
-        for k in G:
-            if i in k:
-                break
-        else:
-            start_case.append(i)
+    # start_case = []
+    # for i in range(1, V+1):
+    #     if not G[i]:
+    #         start_case.append(i)
+        # for k in G:
+        #     if i in k:
+        #         break
+        # else:
+        #     start_case.append(i)
 
-    input_count = []
-    for j in range(0, V + 1):
-        count = 0
-        for k in G:
-            if j in k:
-                count += 1
-        input_count.append(count)
 
-    result = ''
+    # input_count = []
+    # # for j in range(0, V + 1):
+    # #     input_count.append(len(G[j]))
+    # for j in range(0, V + 1):
+    #     count = 0
+    #     for k in G:
+    #         if j in k:
+    #             count += 1
+    #     input_count.append(count)
+
     v = start_case.pop()
     visit[v] = 1
     S = [v]
+    result = [str(v)]
+    # result = []
+    while S or start_case:
+        # if not S and not start_case:
+        #     break
 
-    while S:
-        for w in G[v]:
-            if visit[w] + 1 == input_count[w]:
-                print('{} -> {}'.format(v, w))
+        if not S:
+            v = start_case.pop()
+            result.append(str(v))
+            visit[v] = 1
+            S = [v]
+
+        for i in range(len(G[v])):
+            w = G[v][i]
+            if visit[w] + 1 == input_count[w] and w:
+
+                # if v not in result:
+                #     result.append(str(v))
+                if str(w) not in result:
+                    result.append(str(w))
+
+                # print('{} -> {}'.format(v, w))
                 visit[w] += 1
-                S.append(w)
+                S.append(v)
                 v = w
                 break
-            else:
+            elif visit[w]+1 != input_count[w] and input_count[w] > 1 and w:
                 visit[w] += 1
-                v = S.pop()
-                continue
-
+                G[v][i] = 0
+                # break  
         else:
-            if S:
-                v = S.pop()
+            v = S.pop()
 
-
-        # if not S:
-        #     v = start_case.pop()
-        #     S = [v]
+    print('#{} {}'.format(tc, ' '.join(result)))
+    # print(len(result))
+    # print('V=', V)
+    # print(visit)
 
 
 
