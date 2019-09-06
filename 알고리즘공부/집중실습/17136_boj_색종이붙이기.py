42eff80b62b9174f9b5bb583e881d11905d68b7d
ByeongjunCho <jjgk91@naver.com>
2019-09-06 17:53:58 +0900

    문제풀이

--- 알고리즘공부/집중실습/17136_boj_색종이붙이기.py
+++ 알고리즘공부/집중실습/17136_boj_색종이붙이기.py
@@ -1,42 +1,41 @@
 arr = [list(map(int, input().split())) for _ in range(10)]
+paper = [0, 5, 5, 5, 5, 5]
+one = []  # 1의 좌표
 
-five = [[1 for _ in range(5)] for _ in range(5)]
-four = [[1 for _ in range(4)] for _ in range(4)]
-three = [[1 for _ in range(3)] for _ in range(3)]
-two = [[1 for _ in range(2)] for _ in range(2)]
 
-case = [five, four, three, two]
-test_case = [5, 5, 5, 5]
-for _ in range(4):
-    for idx, c in enumerate(case):
-        all_flag = 0
-        slide = 10 - len(c) + 1
-        for row in range(slide):
-            for col in range(slide):
-                inner_flag = 0
-                if not test_case[idx]:
-                    all_flag = 1
-                    break
-                for i in range(len(c)):
-                    for j in range(len(c)):
-                        if arr[row+i][col+j] != c[i][j]:
-                            inner_flag = 1
-                    if inner_flag == 1:
-                        break
-                else:
-                    for i in range(len(c)):
-                        for j in range(len(c)):
-                            arr[row+i][col+j] = 0
-                    test_case[idx] -= 1
-            if all_flag:
+# 1. 찾는다
+# 2. 지운다. 종이 사용
+# 3. 다시 복구
+
+
+def isPaper(row, col, mode):
+    if row + mode > 10 or col + mode > 10:
+        return False
+    for i in range(row, row+mode):
+        for j in range(col, col+mode):
+            if not arr[i][j]:
+                return False
+
+    return True
+
+def setPaper(row, col, mode, val):
+    for i in range(row, row+mode):
+        for j in range(col, col+mode):
+            arr[row][col] = val
+
+def paper():
+
+    for mode in range(5, 0, -1):
+        # 색종이를 5부터 1까지 확인
+        for i in range(10):
+            if not paper[mode]:
                 break
-one = 0
-for i in range(10):
-    for j in range(10):
-        if arr[i][j]:
-            one += arr[i][j]
-
-if one > 5:
-    print(-1)
-else:
-    print(20 - sum(test_case) + one)
+            for j in range(10):
+                if isPaper(i, j, mode):
+                    if not paper[mode]:
+                        break
+                    paper[mode] -= 1
+                    setPaper(i, j, mode, 0)
+                    paper()
+                    paper[mode] += 1
+                    setPaper(i, j, mode, 1)
