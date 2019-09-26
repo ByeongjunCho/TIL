2379f4fcda883546f8e6569e9f9891b285cb48a8
ByeongjunCho <jjgk91@naver.com>
2019-09-26 23:10:16 +0900

    190926 | SWEA 백트랙킹 문제풀이중

---
+++ 알고리즘공부/분할정복백트랙킹/5208_전기버스2.py
@@ -0,0 +1,19 @@
+def bus2(k, start):
+    global result
+    if k > result:
+        return
+    if start == arr[0] - 1:
+        result = min(result, k)
+        return
+    for i in range(start + arr[start], start, -1):
+        if i > arr[0] - 1:
+            continue
+        bus2(k + 1, i)
+
+T = int(input())
+for tc in range(1, T+1):
+    arr = list(map(int, input().split()))
+    result = 100
+    bus2(-1, 1)
+
+    print('#{} {}'.format(tc, result))
