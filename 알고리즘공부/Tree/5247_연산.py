b846dd253f14aba8d38104fe527c6e51f51f81b3
ByeongjunCho <jjgk91@naver.com>
2019-10-16 18:03:16 +0900

    191016 | 최소신장트리

--- 알고리즘공부/Tree/5247_연산.py
+++ 알고리즘공부/Tree/5247_연산.py
@@ -1,6 +1,30 @@
-T = int(input())
+from collections import deque
+def calc(s, e):
+    MAX = 1000001
 
+    dp = [0 for _ in range(MAX)]
+    dp[s] = 0
+    Q = deque()
+    Q.append(s)
+
+    while True:
+        i = Q.popleft()
+        if 0 <= i+1 < MAX and not dp[i+1]:
+            dp[i+1] = dp[i]+1
+            Q.append(i+1)
+        if 0 <= i - 1 < MAX and not dp[i-1]:
+            dp[i-1] = dp[i]+1
+            Q.append(i-1)
+        if 0 <= 2*i < MAX and not dp[i*2]:
+            dp[i*2] = dp[i]+1
+            Q.append(i*2)
+        if 0 <= i-10 < MAX and not dp[i-10]:
+            dp[i-10] = dp[i]+1
+            Q.append(i - 10)
+        if dp[e]:
+            return dp[e]
+
+T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
-    used = (1, -1, 2, -10)
-    dp = [[1000001 for _ in range(M+1)] for _ in range(5)]
+    print('#{} {}'.format(tc, calc(N, M)))
---
+++ 알고리즘공부/Tree/간선 완화.py
@@ -0,0 +1,65 @@
+from collections import deque
+# 가중치가 없는 경우 BFS를 이용한 거리 계산
+def BFS(s):
+    D = [0] * (V+1)
+    visit = [False] * (V+1)
+    Q = deque()
+    Q.append(s); visit[s] = True; D[s] = 0
+
+    while Q:
+        u = Q.popleft()
+        for v in G[u]:
+            if not visit[v]:
+                Q.append(v)
+                visit[v] = True
+                D[v] += D[u] + 1
+
+# 가중치가 존재하는 경우 => BFS를 이용한 brute force 방법
+def BFS(s):
+    D = [0xffffff] * (V + 1) # D[] 초기값 설정
+    # visit = [False] * (V + 1)
+    Q = deque()
+    Q.append(s);
+    D[s] = 0
+
+    while Q:
+        u = Q.popleft()
+        for v, w in G[u]:
+            if D[v] > D[u] + w: # u -> v
+                D[v] += D[u] + w
+                Q.append(v)
+
+# 가중치가 존재하는 경우 => djikstra를 이용한 방법
+
+
+def BFS(s):
+    D = [0xffffff] * (V + 1) # D[] 초기값 설정
+    visit = [False] * (V + 1) # 이미 최단 경로를 찾은 정점들과 아닌 정점들 구분
+    D[s] = 0
+    cnt = V
+    while cnt:
+        #아직 최단경로를 찾지 못한 정점 중에서 D[]가 최소인 정점을 찾는다.
+        MIN = 0xffffff
+        for i in range(V+1):
+            if not visit[i] and MIN < D[i]:
+                MIN = D[i]
+                idx = i
+
+        for v, w in G[idx]:
+            if D[idx] + w < D[v]:
+                D[v] += D[idx] + w
+                visit[v] = True
+
+        cnt -= 1
+
+
+
+
+V, E = map(int, input().split())
+G = [[] for _ in range(V)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    G[u].append((v, w))
+    G[v].append((u, w))
+
+BFS(0)
