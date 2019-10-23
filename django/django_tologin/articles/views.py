69fc2d58779bfa05983de5f40f7a53175cc09b46
ByeongjunCho <jjgk91@naver.com>
2019-10-23 17:56:04 +0900

    191023 | login구현

--- django/django_tologin/articles/views.py
+++ django/django_tologin/articles/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 # Create your views here.
 
 def index(request):
     return render(request, 'articles/index.html', {'articles': Article.objects.order_by('-id')})
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return redirect('articles:index')
+    else:
+        form = ArticleForm()
+    return render(request, 'articles/create.html', {'form': form})
+
+def detail(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    return render(request, 'articles/detail.html', {'article': article})
+
+def update(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    if request.method == 'post':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = ArticleForm(instance=article)
+    return render()
