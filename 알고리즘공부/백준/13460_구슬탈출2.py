dc0cec7ea60e1d956283c6b259c4ddb33b91813e
ByeongjunCho <jjgk91@naver.com>
2019-11-04 22:52:09 +0900

    구슬넣기2 풀이

--- 알고리즘공부/백준/13460_구슬탈출2.py
+++ 알고리즘공부/백준/13460_구슬탈출2.py
@@ -5,7 +5,7 @@ def go(row, col, color, status):
         i, j = row, col
         arr[i][j] = '.'
         while True:
-            if 0 <= i-1 < N and arr[i - 1][j] == 'O':
+            if 0 <= i-1 < N and (i-1, j) == hole:
                 return (i-1, j)
             elif 0 <= i-1 < N and arr[i - 1][j] == '.':
                 i -= 1
@@ -17,7 +17,7 @@ def go(row, col, color, status):
         i, j = row, col
         arr[i][j] = '.'
         while True:
-            if 0 <= j+1 < M and arr[i][j+1] == 'O':
+            if 0 <= j+1 < M and (i, j+1) == hole:
                 return (i, j+1)
             elif 0 <= j+1 < M and arr[i][j+1] == '.':
                 j += 1
@@ -29,7 +29,7 @@ def go(row, col, color, status):
         i, j = row, col
         arr[i][j] = '.'
         while True:
-            if 0 <= i+1 < N and arr[i + 1][j] == 'O':
+            if 0 <= i+1 < N and (i+1, j) == hole:
                 return (i+1, j)
             elif 0 <= i+1 < N and arr[i + 1][j] == '.':
                 i += 1
@@ -41,7 +41,7 @@ def go(row, col, color, status):
         i, j = row, col
         arr[i][j] = '.'
         while True:
-            if 0 <= j-1 < M and arr[i][j-1] == 'O':
+            if 0 <= j-1 < M and (i, j-1) == hole:
                 return (i, j-1)
             elif 0 <= j-1 < M and arr[i][j-1] == '.':
                 j -= 1
@@ -78,7 +78,7 @@ def ball(red, blue, status): # 1234=>위오아왼
             r = go(red[0], red[1], 'R', status)
             b = go(blue[0], blue[1], 'B', status)
             return r, b
-    else:
+    else:  # 왼
         if red[1] > blue[1]:
             b = go(blue[0], blue[1], 'B', status)
             r = go(red[0], red[1], 'R', status)
@@ -90,7 +90,7 @@ def ball(red, blue, status): # 1234=>위오아왼
 
 N, M = map(int, (input().split()))
 arr = [list(input()) for _ in range(N)]
-
+result = 100
 # 파라미터 좌표
 
 for i in range(N):
@@ -102,8 +102,31 @@ for i in range(N):
         elif arr[i][j] == 'O':
             hole = (i, j)
 
-def play(k, br, bb, ar, ab):
+
+
+def play(arr, k, br, bb, ar, ab):
+    global result
+    if ar == hole and ab != hole:
+        result = min(result, k)
+        return
+    elif ab == hole:
+        return
+    elif k > 10:
+        return
+    elif br == ar and bb == ab:
+        return
+    elif k > result:
+        return
 
     for status in range(1, 5):
-        r, b = ball(br, bb, status)
-        play(k+1, )
+        r, b = ball(ar, ab, status)
+        play(arr, k+1, ar, ab, r, b)
+        # 공을 제자리로 돌린다.
+        arr[r[0]][r[1]] = '.'
+        arr[ar[0]][ar[1]] = 'R'
+        arr[b[0]][b[1]] = '.'
+        arr[ab[0]][ab[1]] = 'B'
+
+
+play(arr, 0, (-1, -1), (-1, -1), red, blue)
+print(result if result < 11 else -1)
